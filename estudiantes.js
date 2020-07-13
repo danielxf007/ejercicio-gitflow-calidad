825ba63162a6d38879cf068a809bdd10a7ce0047
danielxf007 <daniel.higuita007@gmail.com>
2020-07-12 23:21:39 -0500

    Cambios Daniel H

--- estudiantes.js
+++ estudiantes.js
@@ -3,6 +3,6 @@ const LISTA_ESTUDIANTES = [
         nombre: 'Daniel Alejandro Higuita Usuga',
         sede: 'Medellín',
         github: 'danielxf007',
-        page: 'https://www.youtube.com/watch?v=2TMs2E6cms4'
+        page: './pages/DanielHiguita.html'
     }
 ];
